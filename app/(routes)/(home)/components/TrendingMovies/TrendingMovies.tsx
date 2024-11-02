0e3ec1b7f16f4d28fddd6107b493579c60d1e20d
williams123000 <williamschan72@gmail.com>
2024-11-01 18:59:50 -0600

    Se arreglo para movil el trending movies

--- app/(routes)/(home)/components/TrendingMovies/TrendingMovies.tsx
+++ app/(routes)/(home)/components/TrendingMovies/TrendingMovies.tsx
@@ -1,10 +1,13 @@
+"use client";
+
 import Image from "next/image";
 import { TrendingMoviesProps } from "./TrendingMovies.types";
 import { InfoExtraFilm } from "./InfoExtraFilm";
+import { useRouter } from "next/navigation";
 
 export function TrendingMovies(props: TrendingMoviesProps) {
   const { movies } = props;
-
+  const router = useRouter();
   return (
     <div className="pt-11 md:pt-0 md:-top-24 lg:-top-28 relative px-[4%]">
       <h3 className="text-2xl font-semibold mb-3">
@@ -21,7 +24,7 @@ export function TrendingMovies(props: TrendingMoviesProps) {
               <div
                 className="flex transition duration 
               group-hover:opacity-90 delay-300 w-full justify-center"
-              >
+              onClick={() => router.push(`/movie/${movie.id}`)}             >
                 <Image
                   src={`https://raw.githubusercontent.com/ratasi/images-netflix-clone/refs/heads/main/ranking/${movie.ranking}.png`}
                   alt="Number"
@@ -35,6 +38,7 @@ export function TrendingMovies(props: TrendingMoviesProps) {
                   width={116}
                   height={150}
                   className="h-auto w-auto md:max-h-[180px] lg:max-h-full"
+                  
                 />
               </div>
               <InfoExtraFilm movie={movie} />
